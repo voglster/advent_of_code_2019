b9018ecc2767294c511924a283827ac4ba9b047b
James Vogel <jim.m.vogel@gmail.com>
2019-12-13 20:59:54 -0600

    d4p2

--- d4/d4.py
+++ d4/d4.py
@@ -24,3 +24,29 @@ for i in range(low, high + 1):
         count += 1
 
 print(count)
+
+
+def has_double_digit_strict(number):
+    num = str(number)
+    good_chars = set()
+    last_char = None
+    count = 0
+    for char in num:
+        if last_char == char:
+            count += 1
+            if count == 2:
+                good_chars.add(char)
+            if count == 3:
+                good_chars.remove(char)
+        else:
+            count = 1
+            last_char = char
+    return good_chars
+
+
+count = 0
+for i in range(low, high + 1):
+    if digits_dont_decrease(i) and has_double_digit_strict(i):
+        count += 1
+
+print(count)
